ddf01d1669ee7d51fa27e364422ad80b14933bce
jpwarren13 <johnpwarren@gmail.com>
2018-09-19 15:58:28 -0500

    queue.js working

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,24 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+  storage[Object.keys(storage).length] = value; 
   };
 
   someInstance.dequeue = function() {
+  var firstElement = storage["0"]; 
+  delete storage["0"];
+  for (var key in storage){
+     var tempKey = key - 1;
+     storage[tempKey] = storage[key]; 
+    }
+  
+  delete storage[Object.keys(storage).length-1];
+
+  return firstElement; 
   };
 
   someInstance.size = function() {
+  return Object.keys(storage).length;
   };
 
   return someInstance;
